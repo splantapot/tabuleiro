3fe313cbfb78514eeb297681412778b9eaf9dc17
splantapot <109237501+splantapot@users.noreply.github.com>
2024-03-13 09:09:22 -0300

    Segundo up

--- js/classes/Cards.js
+++ js/classes/Cards.js
@@ -1,16 +1,38 @@
 class Cards {
-    constructor(id, deck, life, attack, range, dominio) {
+    constructor(id, deck, name,power, life) {
         this.id = id;
         this.deck = deck;
+        this.name = name;
         this.life = life;
-        this.attack = attack;
-        this.range = range;
-        this.dominio = dominio;
+        this.power = power;
 
         this.cardScale = (1080/1650); // =~ 0.6545
 
         let newPlace = document.createElement('div');
         newPlace.id = id;
         newPlace.classList.add('card');
+        newPlace.style.width = `${60}px`
+        newPlace.style.height = `${60/this.cardScale}px`;
+
+        /*let imagemCarta = document.createElement('img');
+        imagemCarta.src = `./cartas/${id}.jpg`
+        newPlace.appendChild(imagemCarta);*/
+
+
+        cardBox.appendChild(newPlace);
     }
+}
+
+function makeCards(str = '', deck = '') {
+    let cartText = str.trim().split(';');
+    cartText.forEach((cardText) => {
+        cards.push(new Cards(
+            `${cards.length}_card`, //id
+            deck, //Deck
+            cardText.trim().split('/')[0], //Name
+            parseInt((cardText.trim().split('/')[1])), //Power
+            parseInt((cardText.trim().split('/')[2])) //Life
+        ))
+    });
+    console.log(cards);
 }
--- js/main.js
+++ js/main.js
@@ -19,3 +19,16 @@ for (let y = 0; y < placeNumbers; y++) {
         places.push(new Place(`${(y*10)+x}_place`, placeSize/placeNumbers, cor, x, y));
     }
 }
+
+let cards = [];
+
+makeCards(`
+    Zumbí dos Palmares/80/150;
+    Esperança Garcia/60/200;
+    Edson Arantes (Pelé)/90/150;
+    Dom Pedro II/50/130;
+    Machado de Assis/70/100;
+    Tarsila do Amaral/80/130;
+    Getúlio Vargas/80/100;
+    Carmem Miranda/80/160;  
+`, 'Brasil')
