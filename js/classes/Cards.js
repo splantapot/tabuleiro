793bf29817d5f2fdfccfe009a14ace72b9ec5afb
splantapot <109237501+splantapot@users.noreply.github.com>
2024-03-21 11:49:18 -0300

    Up 2103

--- js/classes/Cards.js
+++ js/classes/Cards.js
@@ -121,7 +121,7 @@ function cardSelectable(elmnt = document.getElementById('elmnt')) {
     function onMouseDown(e) {
         e == e || window.event;
         e.preventDefault();
-
+        
         //Movement
         if (selected.id == null && game.phase == 0) {
             selected.id = parseInt(e.target.id.split('_')[0]);
@@ -129,7 +129,7 @@ function cardSelectable(elmnt = document.getElementById('elmnt')) {
             selected.origin = document.getElementById(selected.id+'_carta').offsetParent.id;
             const cardSelected = cards[selected.id];
             cardSelected.div.classList.add('select');
-
+            
             if (document.getElementById(selected.origin).classList.contains('place')) {
                 const org = parseInt(selected.origin.split('_')[0]);
                 const maxX = org-(org%placeNumbers)+placeNumbers-1;
@@ -164,11 +164,11 @@ function cardSelectable(elmnt = document.getElementById('elmnt')) {
                                 }
                             }
                         }
-                    break;
-
-                    case '*':
-                        for(let dir = 0; dir < 8; dir++) {
-                            add.x = Math.round(Math.cos(dir*Math.PI/4));
+                        break;
+                        
+                        case '*':
+                            for(let dir = 0; dir < 8; dir++) {
+                                add.x = Math.round(Math.cos(dir*Math.PI/4));
                             add.y = Math.round(Math.sin(dir*Math.PI/4));
                             for (let m = 1; m<=cardSelected.moveRng; m++) {
                                 const posGain = ((add.x*m)+(add.y*m*placeNumbers)+org);
@@ -177,23 +177,79 @@ function cardSelectable(elmnt = document.getElementById('elmnt')) {
                                 }
                             }
                         }
-                    break;
+                        break;
+                    }
                 }
             }
-        }
-
-        //Attack
-        if (selected.id == null && game.phase == 1) {
-            selected.id = parseInt(e.target.id.split('_')[0]);
+            
+            //Attack
+            if (selected.id == null && game.phase == 1) {
+                selected.id = parseInt(e.target.id.split('_')[0]);
             selected.time = new Date().getTime();
             const cardSelected = cards[selected.id];
             cardSelected.div.classList.add('atk');
         }
+        if (selected.id != null && game.phase == 1 && selected.id != parseInt(e.target.id.split('_')[0])) {
+            const targetId = parseInt(e.target.id.split('_')[0]);
+            const targetCard = cards[targetId];
+            const atackerCard = cards[selected.id]
+            const dmgType = Math.sign(atackerCard.power-targetCard.power);
+            const dmgLabel = document.getElementById('boardDmg');
+            console.log(`${atackerCard.name} atk -> ${targetCard.name} (${dmgType})` );
+            let finalPos;
+            
+            switch (dmgType) {
+                case 1:
+                    finalPos = {
+                        x: Math.round(targetCard.div.getBoundingClientRect().x),
+                        y: Math.round(targetCard.div.getBoundingClientRect().y),
+                        w: Math.round(targetCard.div.getBoundingClientRect().width),
+                        h: Math.round(targetCard.div.getBoundingClientRect().height)
+                    };
+                    dmgLabel.innerHTML = `-${atackerCard.power-targetCard.power}`;
+                    dmgLabel.style.left = `${finalPos.x-finalPos.w}px`;
+                    dmgLabel.style.top = `${finalPos.y}px`;
+                    dmgLabel.style.display = 'block';
+                    dmgLabel.classList.add('dmgUp');
+                    targetCard.life -= (atackerCard.power-targetCard.power);
+                    targetCard.div.classList.add('dmgTake');
 
+                break;
+                case -1:
+                    finalPos = {
+                        x: Math.round(atackerCard.div.getBoundingClientRect().x),
+                        y: Math.round(atackerCard.div.getBoundingClientRect().y),
+                        w: Math.round(atackerCard.div.getBoundingClientRect().width),
+                        h: Math.round(atackerCard.div.getBoundingClientRect().height)
+                    };
+                    dmgLabel.innerHTML = `-${targetCard.power-atackerCard.power}`;
+                    dmgLabel.style.left = `${finalPos.x-finalPos.w}px`;
+                    dmgLabel.style.top = `${finalPos.y}px`;
+                    dmgLabel.style.display = 'block';
+                    dmgLabel.classList.add('dmgUp');
+                    atackerCard.life -= (targetCard.power-atackerCard.power);
+                    atackerCard.div.classList.add('dmgTake');
+                break;
+            }
+
+            setTimeout(() => {
+                atackerCard.div.classList.remove('dmgTake');
+                targetCard.div.classList.remove('dmgTake');
+                dmgLabel.classList.remove('dmgUp');
+                dmgLabel.style.display = 'none'
+            }, 999);
+            defaultClean();
+        }
+        
         if (new Date().getTime()-selected.time>180 && selected.id != null && (
             (selected.id == parseInt(elmnt.id.split('_')[0])) ||
-            (selected.id != elmnt.id && elmnt.classList.contains('card'))
+            (selected.id != elmnt.id && elmnt.classList.contains('card') && game.phase==0)
             )){
+                defaultClean();
+            }
+
+            
+        function defaultClean() {
             const cardSelected = cards[selected.id];
             cardSelected.div.classList.remove('select');
             cardSelected.div.classList.remove('atk')
@@ -209,7 +265,6 @@ function cardSelectable(elmnt = document.getElementById('elmnt')) {
                 selected.editPlaces = null;
             }
         }
-
         elmnt. onmouseup = onMouseUp;
     };
 
