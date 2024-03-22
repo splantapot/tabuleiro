8c3d5c8a5e0a05e2a81abbe29e36fef21ed9be84
splantapot <109237501+splantapot@users.noreply.github.com>
2024-03-22 11:58:45 -0300

    Update 11e58

--- js/gameStatus.js
+++ js/gameStatus.js
@@ -5,7 +5,7 @@
 */
 const game = {
     phase:0,
-    phaseMax:1,
+    phaseMax:2,
     phaseTitle:'Fase de Movimento',
     turnActs:0,
     turnPlayer:0,
@@ -13,25 +13,34 @@ const game = {
 }
 
 function proxFase() {
-    if (selected.id == null) {
-        game.phase++;
-        if (game.phase > game.phaseMax) {
-            resetTurno();
-        }
-    
-        switch (game.phase) {
-            case 0:
-                game.phaseTitle = 'Fase de Movimento'
-            break;
-            case 1:
-                game.phaseTitle = 'Fase de Ataque'
-            break;
-            case 2:
-                game.phaseTitle = 'Fase de Acordos'
-            break;
-        }
-        document.getElementById('boardPhase').innerHTML = game.phaseTitle;
+
+    if (selected.id != null) {
+        cards[selected.id].div.classList.remove('select');
+        cards[selected.id].div.classList.remove('atk');
+        selected.id = null;
+        selected.id = null;
+        selected.time = new Date().getTime();
+        selected.origin = null;
+        cleanRangeList();
     }
+
+    game.phase++;
+    if (game.phase > game.phaseMax) {
+        resetTurno();
+    }
+    
+    switch (game.phase) {
+        case 0:
+            game.phaseTitle = 'Fase de Movimento'
+        break;
+        case 1:
+            game.phaseTitle = 'Fase de Ataque'
+        break;
+        case 2:
+            game.phaseTitle = 'Fase de Acordos'
+        break;
+    }
+    document.getElementById('boardPhase').innerHTML = game.phaseTitle;
 }
 
 function resetTurno() {
